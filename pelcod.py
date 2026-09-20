a3a8000f0476bf9cdc43e82eaa623c8b61a7295d
tresnahizkialumbantobing-prog <tresnahizkialumbantobing@gmail.com>
2026-09-20 08:50:41 +0700

    tresna

---
+++ pelcod.py
@@ -0,0 +1,24 @@
+nama = "Renatha" 
+
+print (nama)
+
+umur = 29
+
+angka1 = 15
+angka2 = 5
+angka3 = 8
+angka4 = 2
+angka5 = 4
+angka6 = 3
+
+print(angka1 / angka2)
+print(angka3 + angka4)
+print(angka5 // angka6)
+
+x = 5
+print(x > 2 and x < 10)
+
+x = 5
+print(x < 2 or x < 10)
+
+x = 5 
