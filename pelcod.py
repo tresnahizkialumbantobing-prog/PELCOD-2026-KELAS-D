e06a4440af48cc7259fe582ca6baa2defdceed9e
tresnahizkialumbantobing-prog <tresnahizkialumbantobing@gmail.com>
2026-09-23 10:20:56 +0700

    260404100112_Tresna_Hizkia_Lumban_Tobing

--- pelcod.py
+++ pelcod.py
@@ -1,24 +1,25 @@
-nama = "Renatha" 
+nama = "Renatha"
+nilai_tugas = 100
+nilai_kuis = 100
+nilai_ujian = 90
+kehadiran = 100
 
-print (nama)
+nilai_akhir =  (nilai_tugas * 30 / 100) + (nilai_kuis * 20 / 100) + (nilai_ujian * 50 / 100)
 
-umur = 29
+if kehadiran < 75:
+   status = "Tidak lulus"
+elif nilai_akhir >= 85 and kehadiran >= 80:
+   status = "Lulus dengan predikat A"
+elif nilai_akhir >= 75 and kehadiran >= 80:
+   status = "Lulus dengan predikat B"
+elif nilai_akhir >= 65 and kehadiran >= 75:
+   status = "Lulus dengan predikat C"
+else:
+     status = "Tidak lulus"
 
-angka1 = 15
-angka2 = 5
-angka3 = 8
-angka4 = 2
-angka5 = 4
-angka6 = 3
-
-print(angka1 / angka2)
-print(angka3 + angka4)
-print(angka5 // angka6)
-
-x = 5
-print(x > 2 and x < 10)
-
-x = 5
-print(x < 2 or x < 10)
-
-x = 5 
+print()
+print("=====HASIL PENILAIAN=====")
+print("Nama         :" , nama)
+print("Nilai_akhir  :" , nilai_akhir)
+print("Kehadiran   :", kehadiran, "%")
+print("Status      :", status)
